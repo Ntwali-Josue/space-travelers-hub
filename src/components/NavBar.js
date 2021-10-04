4b141506c9b39a992d2b552b35c1d947847038aa
Ntwali-Josue <kntwalijos@gmail.com>
2021-10-04 20:16:57 +0200

    Add components folder

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../images/logo.png';
+
+const NavBar = () => (
+  <>
+    <img src={logo} className="logo" alt="Logo" />
+    <h1> NavBar</h1>
+  </>
+);
+
+export default NavBar;
