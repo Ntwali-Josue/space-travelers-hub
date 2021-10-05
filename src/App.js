d815cc003f524524234f7d3e6b0c60d3d5bb35a0
Ángel Díaz <diazangel9311@gmail.com>
2021-10-05 12:16:47 -0500

    Provider now wraps App in index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/configureStore';
 import NavBar from './components/NavBar';
 import MissionsPage from './components/missions/MissionsPage';
 import RocketsPage from './components/rockets/RocketsPage';
@@ -10,25 +8,23 @@ import NoMatch from './components/NoMatch';
 import './App.css';
 
 const App = () => (
-  <Provider store={store}>
-    <Router basename={process.env.PUBLIC_URL}>
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <RocketsPage />
-        </Route>
-        <Route path="/missions">
-          <MissionsPage />
-        </Route>
-        <Route path="/profile">
-          <ProfilePage />
-        </Route>
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
-    </Router>
-  </Provider>
+  <Router basename={process.env.PUBLIC_URL}>
+    <NavBar />
+    <Switch>
+      <Route exact path="/">
+        <RocketsPage />
+      </Route>
+      <Route path="/missions">
+        <MissionsPage />
+      </Route>
+      <Route path="/profile">
+        <ProfilePage />
+      </Route>
+      <Route path="*">
+        <NoMatch />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default App;
