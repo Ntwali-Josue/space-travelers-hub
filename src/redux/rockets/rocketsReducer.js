01260ae6d079e472dfe0984f01421ab2b9412cb0
Ángel Díaz <diazangel9311@gmail.com>
2021-10-05 15:10:09 -0500

    Add dispatch action to reserve a rocket

--- src/redux/rockets/rocketsReducer.js
+++ src/redux/rockets/rocketsReducer.js
@@ -1,19 +1,39 @@
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-// import fetchAPI from '../fetchAPI';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import fetchAPI from '../fetchAPI';
 
-const FETCH_ROCKETS = 'spaceX/rockets/FETCH_ROCKETS/';
+const FETCH_ROCKETS = 'spaceX/rockets/FETCH_ROCKETS/fulfilled';
+const RESERVE_ROCKET = 'spaceX/rockets/RESERVE_ROCKET';
 
-const initialState = [];
+const initialState = {
+  status: 'empty',
+  rocketList: [],
+  reservedRockets: [],
+};
 
-export const fetchRockets = (payload) => ({
-  type: FETCH_ROCKETS,
+export const fetchRocket = createAsyncThunk('spaceX/rockets/FETCH_ROCKETS', async () => {
+  const response = await fetchAPI('https://api.spacexdata.com/v3/rockets');
+  return response;
+});
+
+export const reserveRocket = (payload) => ({
+  type: RESERVE_ROCKET,
   payload,
 });
 
 const rocketsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_ROCKETS:
-      return [...state, action.payload];
+      return {
+        status: 'fetched',
+        rocketList: [...state.rocketList, ...action.payload],
+        reservedRockets: [],
+      };
+    case RESERVE_ROCKET:
+      return {
+        status: state.status,
+        rocketList: state.rocketList,
+        reservedRockets: [...state.reservedRockets, action.payload],
+      };
     default:
       return state;
   }
