c18b1b2992cca73cecf3ef7a3813e7bcda311716
Ntwali-Josue <kntwalijos@gmail.com>
2021-10-06 14:53:46 +0200

    Add mission page and render mission component

--- src/components/missions/MissionsPage.js
+++ src/components/missions/MissionsPage.js
@@ -1,7 +1,34 @@
-const MissionsPage = () => (
-  <div>
-    <p>Missions... Under construction.</p>
-  </div>
-);
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMission } from '../../redux/missions/missionsReducer';
+import Mission from './Missions';
+
+const MissionsPage = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missionList);
+  const status = useSelector((state) => state.missions.status);
+
+  useEffect(() => {
+    if (status === 'empty') {
+      dispatch(fetchMission());
+    }
+  }, [status]);
+
+  return (
+    <div>
+      {
+        missions.map((mission) => (
+          <Mission
+            key={mission.id}
+            id={mission.mission_id}
+            missionName={mission.mission_name}
+            description={mission.description}
+          />
+        ))
+      }
+      <Mission />
+    </div>
+  );
+};
 
 export default MissionsPage;
