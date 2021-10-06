7e45b41bcb380107b41c7b6bd0347e5914a04522
Ntwali-Josue <kntwalijos@gmail.com>
2021-10-06 14:38:41 +0200

    Add Mission component

---
+++ src/components/missions/Missions.js
@@ -0,0 +1,46 @@
+/* eslint-disable react/prop-types */
+// import { } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Table, Button } from 'react-bootstrap';
+
+const Mission = (props) => {
+  // const dispatch = useDispatch();
+  const { missionName, description } = props;
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th> </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{missionName}</td>
+          <td>{description}</td>
+          <td>Not a member</td>
+          <td>
+            {' '}
+            <Button>Join Mission</Button>
+            {' '}
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+};
+
+Mission.propTypes = {
+  missionName: PropTypes.string,
+  description: PropTypes.string,
+};
+
+Mission.defaultProps = {
+  missionName: '',
+  description: '',
+};
+
+export default Mission;
