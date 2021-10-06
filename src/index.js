388bf77f40ebb24953d16f036279eefc72caa3e7
Ntwali-Josue <kntwalijos@gmail.com>
2021-10-06 14:53:16 +0200

    Import store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/configureStore';
+import store from './redux/cofigureStore';
 import App from './App';
 
 ReactDOM.render(
