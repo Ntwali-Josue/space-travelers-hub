f5a8b57efaa70edc6c6f10a758b3e8a07ca96395
Ntwali-Josue <kntwalijos@gmail.com>
2021-10-06 15:02:20 +0200

    Add join mission reducer

--- src/redux/missions/missionsReducer.js
+++ src/redux/missions/missionsReducer.js
@@ -3,15 +3,22 @@ import fetchAPI from '../fetchAPI';
 // import axios from 'axios'
 
 const FETCH_MISSIONS = 'spaceX/missions/FETCH_MISSIONS/fulfilled';
+const JOIN_MISSION = 'spaceX/missions/FETCH_MISSIONS';
 
 const initialState = {
   status: 'empty',
   missionList: [],
+  joinMission: [],
 };
 
 export const fetchMission = createAsyncThunk('spaceX/missions/FETCH_MISSIONS', async () => {
   const result = await fetchAPI('https://api.spacexdata.com/v3/missions');
   return result;
+});
+
+export const joinMisssion = (payload) => ({
+  type: JOIN_MISSION,
+  payload,
 });
 
 const missionsReducer = (state = initialState, action) => {
@@ -21,6 +28,13 @@ const missionsReducer = (state = initialState, action) => {
         status: 'fetched',
         missionList: [...state.missionList, ...action.payload],
       };
+    case JOIN_MISSION:
+      return {
+        status: state.status,
+        missionList: state.missionList,
+        joinMission: [...state.joinMission, action.payload],
+      };
+
     default:
       return state;
   }
