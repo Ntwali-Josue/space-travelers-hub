d65278a248a75c180e746b9f197ff8d1a5ce93dc
Ángel Díaz <diazangel9311@gmail.com>
2021-10-07 12:34:06 -0500

    Add test for fetching rockets

--- src/__tests__/rockets.test.js
+++ src/__tests__/rockets.test.js
@@ -3,6 +3,8 @@ import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
 import rocketsReducer from '../redux/rockets/rocketsReducer';
 import App from '../App';
+import fetchAPI from '../redux/fetchAPI';
+import Rocket from '../components/rockets/Rocket';
 
 describe('Rockets reducer', () => {
   test('initial state', () => {
@@ -43,5 +45,26 @@ describe('Rockets reducer', () => {
       </Provider>
     );
     expect(screen.getByText('Space Travelers Hub')).toMatchSnapshot();
+  });
+
+  test('initial state', async () => {
+    const rocketList = await fetchAPI('https://api.spacexdata.com/v3/rockets');
+    const mapRockets = rocketList.map(
+      (rocket) => (
+        <Rocket
+          key={rocket.id}
+          id={rocket.rocket_id}
+          imgURL={`${rocket.flickr_images[1]}`}
+          name={rocket.rocket_name}
+          description={rocket.description}
+        />
+      ),
+    );
+    render(
+      <Provider store={store}>
+        {mapRockets}
+      </Provider>
+    );
+    expect(screen.getByText('Falcon 1')).toMatchSnapshot();
   });
 });
