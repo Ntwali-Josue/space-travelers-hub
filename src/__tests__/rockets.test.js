cd61d9064622cdd555060051684b8b0554bb6a9d
Ángel Díaz <diazangel9311@gmail.com>
2021-10-07 12:26:59 -0500

    Add test for App

--- src/__tests__/rockets.test.js
+++ src/__tests__/rockets.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
 import rocketsReducer from '../redux/rockets/rocketsReducer';
-import RocketsPage from '../components/rockets/RocketsPage';
+import App from '../App';
 
 describe('Rockets reducer', () => {
   test('initial state', () => {
@@ -30,18 +30,18 @@ describe('Rockets reducer', () => {
     expect(rocketsReducer(initialState, action)).toEqual(
       {
         status: 'fetched',
-        rocketList: [{ name: 'Falcon 1'}, { name: 'Falcon 9'}],
+        rocketList: action.payload,
         reservedRockets: [],
       }
     );
   });
 
-  // test('initial state', () => {
-  //   render(
-  //     <Provider store={store}>
-  //       <RocketsPage />
-  //     </Provider>
-  //   );
-  //   expect(screen.getByText('section')).toMatchSnapshot();
-  // });
+  test('initial state', () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(screen.getByText('Space Travelers Hub')).toMatchSnapshot();
+  });
 });
